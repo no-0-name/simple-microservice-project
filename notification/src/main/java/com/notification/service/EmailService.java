cc264f061088f2bc9945689cdeb842500834ab3f
no-0-name <87208495+no-0-name@users.noreply.github.com>
2025-07-29 02:07:10 +0300

    Update EmailService.java

--- notification/src/main/java/com/notification/service/EmailService.java
+++ notification/src/main/java/com/notification/service/EmailService.java
@@ -8,9 +8,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class EmailService {
     private final JavaMailSender mailSender;
 
